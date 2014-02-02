392dd571eaacce182a8d2ba09a6d6c3d8e213fc1
olivvv <percebois@gmail.com>
2014-02-02 18:45:26 +0100

    fixed jqlite for tab navigation. Lets be careful when this is used in conjuction with jquery, to see if results are consistent

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
 console.log('angular : ', angular);
-angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngMockE2E'])//
+angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngMockE2E'])
 .run(function($httpBackend) {
   $httpBackend.whenGET('api/vegies').respond(
     [{id: 1, name: 'lettuce'}, {id: 2, name: 'carots'}, {id: 3, name: 'pumpkin'}]
@@ -9,27 +9,25 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
     [{id: 1, name: 'brochet'}, {id: 2, name: 'matjes'}, {id: 3, name: 'sandre'}, {id: 4, name: 'turbot'}]
   );
 })
-.factory('vegiesFactory', function($resource){//
- return  $resource('api/vegies');//[{id: 1, name: 'lettuce'}, {id: 2, name: 'carots'}, {id: 3, name: 'pumpkin'}];//
+.factory('vegiesFactory', function($resource){
+ return  $resource('api/vegies');
 })
-.factory('fishesFactory', function($resource){//
- return  $resource('api/fishes');//[{id: 1, name: 'lettuce'}, {id: 2, name: 'carots'}, {id: 3, name: 'pumpkin'}];//
+.factory('fishesFactory', function($resource){
+ return  $resource('api/fishes');
 }) 
 .controller('food', function($scope, vegiesFactory, fishesFactory){
-	//console.log(vegiesFactory);
 	$scope.vegies = vegiesFactory.query();
 	$scope.fishes = fishesFactory.query();
-	//console.log($scope.vegies);
 })
 .directive("dropdown", ['$sce' ,
-				function($sce) { //
+				function($sce) {
 	var DDO = {
 		restrict: 'E',
 		require: '?ngModel',
 		template: '<div ng-click="dropdown.show = !dropdown.show;" tabindex="0" ng-class="{open: dropdown.show, closed: !dropdown.show}" ui-keypress="{\'shift-tab\': \'tab();\', \'tab\': \'tab();\', \'up\': \'up();\', \'down\': \'down($event);\'}"><input type="hidden" ng-model="dropdown.value" /><span ng-bind-html="dropdown.selection" class="selection"></span><ul ng-show="dropdown.show" class="wrap" ng-transclude></ul></div>',
 		transclude: true,
 		scope: true,
-		priority: 100,
+		//priority: 100,
 		controller: ['$scope', '$element', '$timeout',
 			function( $scope,   $element,   $timeout) {
 			$scope.dropdown = {
@@ -40,54 +38,38 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
 
 			this.defaultIsSet = false;
 			$scope.firstOptionSelected = false;
-			var options = $scope.options = [];
+			//var options = 
+			this.options = [];
 			var indexDefault = $scope.indexDefault = 0;
 			this.addOption = function (option) {
-				return options.push(option);
+				return this.options.push(option[0]);
 			};
 
-			/*
-			this.setDefault = function setDefault(rank){
-
-				indexDefault = rank;
-				//ngModel.$setViewValue(options[rank-1]);
-			}*/
-
 			this.select = function select(el, isDefault) {
-				console.warn('DD select', el, isDefault, 'firstOptionSelected', $scope.firstOptionSelected);
 				if (!$scope.firstOptionSelected || isDefault){
 					$scope.firstOptionSelected = true;
-					console.info('DD do select', el, isDefault);
 					$scope.dropdown.selection = $sce.trustAsHtml(el.html());
 					$scope.dropdown.value = el.attr('value');
 				}
 			};
-			/*
-			this.selectRank = function selectRank(rank) {
-				console.log('selectRank ', rank, !this.defaultIsSet, options[rank-1].text());
-				
-				if (this.defaultIsSet === false){
-					this.defaultIsSet = true;
-					console.info('options[rank-1].text() : ', options[rank-1].text());
-					console.log('options[rank-1] : ', options[rank-1]);
-					$scope.dropdown.selection = $sce.trustAsHtml(options[rank-1].html());//'fake';//
-					$scope.dropdown.value = options[rank-1].attr('value');					
-				}
-
-			};*/
 
 			$scope.tab = $scope.up = function(){
-				console.log('tab');
 				$scope.dropdown.show = false;
 			};
 
 			$scope.down = function($event){
-        		console.log('down');
 				$event.preventDefault();
 				$event.stopPropagation();
 				$scope.dropdown.show = true;
 				$timeout(function(){
-					$element.find('ul.wrap li.option').first()[0].focus();
+					var lis = $element.find('li');
+					var focused = false;
+					for (var i = 0, len = lis.length; i < len; i++){
+						if (angular.element(lis[0]).hasClass('option')){
+							lis[0].focus();
+							focused = true;
+						}	
+					}
 				},
 				0);
 			};
@@ -96,36 +78,7 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
 		compile: function compile(el, attr){
 			return {
 				pre: function($scope, el, attr, ngModel) {
-					console.info('link indexDefault : ', $scope);
-					console.info('link $scope.options : ', $scope.options);
-					console.info('link $scope.indexDefault : ', $scope.indexDefault);
-					//$scope.dropdown.selection = $sce.trustAsHtml(options[indexDefault-1].html());//'fake';//
-					
-					//$scope.dropdown.value = $scope.options[$scope.indexDefault].attr('value');	
-					
-					console.info('link $scope.dropdown.value : ', $scope.dropdown.value);
-					
-
-					$scope.$watch('dropdown.value',function (newVal) {
-						console.info('$watch(dropdown.value $scope.options : ', $scope.options);
-						console.info('$watch(dropdown.value $scope.indexDefault : ', $scope.indexDefault);
-						//$scope.dropdown.value = $scope.options[$scope.indexDefault].attr('value');	
-						ngModel.$setViewValue(newVal);
-					});
-				},				
-				post: function($scope, el, attr, ngModel) {
-					console.info('post indexDefault : ', $scope);
-					console.info('post $scope.options : ', $scope.options);
-					console.info('post $scope.indexDefault : ', $scope.indexDefault);
-					//$scope.dropdown.selection = $sce.trustAsHtml(options[indexDefault-1].html());//'fake';//
-					
-					//$scope.dropdown.value = $scope.options[$scope.indexDefault].attr('value');	
-					
-					console.info('post $scope.dropdown.value : ', $scope.dropdown.value);
-					$scope.$watch('dropdown.value',function (newVal) {
-						console.info('post $watch(dropdown.value $scope.options : ', $scope.options);
-						console.info('post $watch(dropdown.value $scope.indexDefault : ', $scope.indexDefault);
-						//$scope.dropdown.value = $scope.options[$scope.indexDefault].attr('value');	
+					$scope.$watch('dropdown.value',function (newVal) {	
 						ngModel.$setViewValue(newVal);
 					});
 				}				
@@ -143,14 +96,11 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
 		template: '<li class="option" tabindex="0" ng-click="selectEl()" ui-keypress="{\'up\': \'up($event);\', \'down\': \'down($event);\'}" ></li>',//
 		transclude: true,
 		replace: true,
-		priority: 90,
+		//priority: 90,
 		scope: true,
 		compile: function(el, attrs){
 			return function(scope, el, attrs, controller,  $transclude) {
 				  $transclude(scope, function(nodes) {
-				    //el.html(''); 
-				  // console.log('transclude o', el[0]);
-				    
 				    el.append(nodes);
 				  }); 
 				console.log('ddli compile attrs', attrs);
@@ -159,6 +109,7 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
 				var state = {text: el.text(), default: attrs.default}; 
 				var dropdown = controller;
 				var rank = dropdown.addOption(el);
+				console.log('ddli dropdown options', dropdown.options);
 			    if (interpolateTextFn){
 			      	console.info('ddli WATCH');
 			        scope.$watch(update, render,true);
@@ -207,6 +158,8 @@ angular.module('testdirective', ['ngResource', 'ngSanitize', 'ui.keypress', 'ngM
 				};
 
 				scope.down = function($event){
+					console.info('down', rank);
+					console.info('down', dropdown.options[rank]);
 					$event.preventDefault();
 					$event.stopPropagation();
 					if (angular.isDefined(dropdown.options[rank])){
